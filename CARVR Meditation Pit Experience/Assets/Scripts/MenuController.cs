e374b818dbb5cc1cbf62c8bd43f3ba686706eacc
Vanessa Tang <vanessatang251@gmail.com>
2025-03-30 00:00:10 -0400

    fixed button connections

---
+++ CARVR Meditation Pit Experience/Assets/Scripts/MenuController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject posesMenu;
+    public GameObject background;
+    public TMPro.TextMeshProUGUI headerText;
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+        posesMenu.SetActive(false);
+        headerText.text = "Main Menu";
+    }
+    public void ShowSettingsMenu()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+        posesMenu.SetActive(false);
+        headerText.text = "Settings";
+    }
+    public void ShowPosesMenu()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        posesMenu.SetActive(true);
+        headerText.text = "Poses";
+    }
+
+    public void HideMenuAfterPoseSelection()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        posesMenu.SetActive(false);
+        background.SetActive(false);
+    }
+}
--- CARVR Meditation Pit Experience/Assets/Scripts/PoseSwitcher.cs
+++ CARVR Meditation Pit Experience/Assets/Scripts/PoseSwitcher.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class PoseSwitcher : MonoBehaviour
 {
-    public GameObject[] poses;
+    public GameObject posesModel;
+    public GameObject defaultPose;
+
+    private GameObject currentPose;
+
+    void Start()
+    {
+        SetDefaultPose();
+    }
 
     public void ActivatePose(GameObject poseToActivate)
     {
-        foreach (GameObject pose in poses)
+        // switches pose
+        if (currentPose != null)
         {
-            pose.SetActive(pose == poseToActivate);
+            currentPose.SetActive(false);
+            Debug.Log("Deactivated pose: " + currentPose.name);
         }
+
+        poseToActivate.SetActive(true);
+        currentPose = poseToActivate;
+        Debug.Log("Activated pose: " + poseToActivate.name);
+    }
+
+    private void SetDefaultPose()
+    {
+        // hides other pose models
+        foreach (Transform pose in posesModel.transform)
+        {
+            pose.gameObject.SetActive(false);
+        }
+
+        if (defaultPose != null)
+        {
+            defaultPose.SetActive(true);
+            currentPose = defaultPose;
+        }
+    }
+
+    public void TestButtonPress()
+    {
+        Debug.Log("Button Pressed!");
     }
 }
